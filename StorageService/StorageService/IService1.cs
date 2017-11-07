36beaff7f83fc7296931d659a9633190e9ce4061
Andriy Markus <andr98m@gmail.com>
2017-11-07 20:09:53 +0200

    Storage created

---
+++ StorageService/StorageService/IService1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace StorageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IStorage
+    {
+        // TODO: Add your service operations here
+        [OperationContract]
+        Product GetDispatchedProducts();
+
+        [OperationContract]
+        bool IsAvailable(int ProductID, int quantity);
+
+        [OperationContract]
+        bool Dispatch(int ProductID, int quantity);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Product
+    {
+        public Product(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+        [DataMember]
+        public int ID { get; private set; }
+
+        [DataMember]
+        public string Name { get; private set; }
+
+        [DataMember]
+        public int Price { get; set; }
+    }
+}
---
+++ StorageService/StorageService/Storage.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace StorageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(
+    ConcurrencyMode = ConcurrencyMode.Single,
+        InstanceContextMode = InstanceContextMode.Single)]
+    public class Storage : IStorage
+    {
+        Product ProductToDispatch;
+        List<ProductInfo> AllProducts;
+        public Storage()
+        {
+            AllProducts = new List<ProductInfo>()
+            {new ProductInfo(){ Id = 1, Name="Product1", Quantity= 39 },
+            new ProductInfo(){ Id = 1, Name="Product2", Quantity= 88 },
+            new ProductInfo(){ Id = 1, Name="Product3", Quantity= 140 },
+            new ProductInfo(){ Id = 1, Name="Product4", Quantity= 15 },
+            new ProductInfo(){ Id = 1, Name="Product5", Quantity= 74 },
+            new ProductInfo(){ Id = 1, Name="Product6", Quantity= 22 } };
+            ProductToDispatch = null;
+        }
+        //Check if desired amount of products ARE
+        public bool IsAvailable(int ProductID, int quantity)
+        {
+           ProductInfo p =  AllProducts.Where(t => t.Id == ProductID).FirstOrDefault();
+            if (p != null && p.Quantity >= quantity)
+                return true;
+            return false;
+        }
+        // return true if Products was successfully Dispatched, otherwise false.
+        // Product dispatched means that it can be taken using "GetDispatchedProduct" method
+        public bool Dispatch(int ProductID, int quantity)
+        {
+            if (IsAvailable(ProductID, quantity))
+            {
+                ProductInfo pinf = AllProducts.Where(t => t.Id == ProductID).FirstOrDefault();
+                Product p = new Product(pinf.Id, pinf.Name);
+                p.Price = 0;
+                pinf.Quantity--;
+                ProductToDispatch = p;
+                return true;
+            }
+            return false;
+        }
+        //return Product, only with Product name, quantity and id
+        public Product GetDispatchedProducts()
+        {
+            Product c = ProductToDispatch;
+            ProductToDispatch = null;
+            return c;
+        }
+    }
+    class ProductInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
